b957bb614d61049b5d4bff037a460ef5812b39d9
Aya Hany <89316483+Ayahany242@users.noreply.github.com>
2024-01-24 04:50:01 +0200

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/authentication/view/AuthenticationActivity.java
@@ -0,0 +1,16 @@
+package com.example.myapplication.authentication.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.myapplication.R;
+
+public class AuthenticationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/authentication/view/SignUpFragment.java
@@ -0,0 +1,34 @@
+package com.example.myapplication.authentication.view;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.myapplication.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link SignUpFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class SignUpFragment extends Fragment {
+    public SignUpFragment() {
+        // Required empty public constructor
+    }
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_sign_up, container, false);
+    }
+}
