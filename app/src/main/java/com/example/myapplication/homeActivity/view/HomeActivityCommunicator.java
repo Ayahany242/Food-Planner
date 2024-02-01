a6991af8a8a78ea613fa15b181394f9062f1f85c
Aya Hany <89316483+Ayahany242@users.noreply.github.com>
2024-02-01 15:27:32 +0200

    Authentication and Home screen with Database

---
+++ app/src/main/java/com/example/myapplication/homeActivity/view/HomeActivityCommunicator.java
@@ -0,0 +1,9 @@
+package com.example.myapplication.homeActivity.view;
+
+import android.content.Context;
+
+public interface HomeActivityCommunicator {
+    public void showToast(String message);
+    public Context getContext();
+
+}
